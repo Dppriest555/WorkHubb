3189aaa9e1d3fcb011eb8ab782ddf57abe75096b
Dppriest555 <ivan.velkov01@abv.bg>
2021-09-08 18:13:28 +0200

    Background color change

--- workhub/src/views/Register.vue
+++ workhub/src/views/Register.vue
@@ -2,13 +2,19 @@
   <div class="regform">
     <div class="error" v-if="error">{{error.message}}</div>
     <form @submit.prevent="pressed">
+      <div class="emailtxt">
+        <h4>Email</h4>
+      </div>
       <div class="email">
-        <input type="email" v-model="email" placeholder="email" />
+        <input type="email"/>
+      </div>
+      <div class="passwordtxt">
+        <h4>Password</h4>
       </div>
       <div class="password">
-        <input type="password" v-model="password" placeholder="password" />
+        <input type="password"/>
       </div>
-      <button type="submit">Register</button>
+      <button type="submit">Sign Up</button>
     </form>
   </div>
 </template>
@@ -44,15 +50,37 @@ export default {
   color: red;
   font-size: 18px;
 }
+.regform{
+  margin: 5px;
+  padding: 30px;
+  margin-top: 40%;
+  border-radius: 10px;
+  background-color: #7460aa13;
+}
+h4 {
+ text-align: left;
+ margin-left: 15px;
+ margin-bottom: 6px;
+ color: #7460AA;
+ font-weight: 500;
+ }
+
+.email {
+  padding-bottom: 20px;
+}
 input {
-  width: 260px;
-  padding: 10px;
-  margin: 8px;
+  width: 90%;
   font-size: 21px;
+  background-color: #7460AA;
+  border-radius: 10px;
+  border: #7460AA;
+  padding: 3px;
 }
 button {
-  width: 150px;
-  height: 75px;
+  margin-top: 20%;
+  height: 40px;
+  width: 30%;
   font-size: 100%;
+  border-radius: 27.5px;
 }
 </style>

0411eaa4cce65b6510b9cf5cb3c749182160d17a
Dppriest555 <ivan.velkov01@abv.bg>
2021-09-08 16:36:20 +0200

    Initial commit

---
+++ workhub/src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import "firebase/firestore"
+
+import dotenv from "dotenv";
+import firebase from 'firebase/app';
+require("firebase/auth");
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXhGdbcPCIMxmhVgCWr9OqyBsx7WTMoBg",
+    authDomain: "projectapp-228ab.firebaseapp.com",
+    projectId: "projectapp-228ab",
+    storageBucket: "projectapp-228ab.appspot.com",
+    messagingSenderId: "681974193584",
+    appId: "1:681974193584:web:a32ac9958d814306e70a75"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+createApp(App).use(store).use(router).mount('#app')
---
+++ workhub/src/views/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="regform">
+    <div class="error" v-if="error">{{error.message}}</div>
+    <form @submit.prevent="pressed">
+      <div class="email">
+        <input type="email" v-model="email" placeholder="email" />
+      </div>
+      <div class="password">
+        <input type="password" v-model="password" placeholder="password" />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import * as firebase from "firebase/app";
+import "firebase/auth";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: ""
+    };
+  },
+  methods: {
+    pressed() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          console.log("here");
+          this.$router.replace({ name: "secret" });
+        })
+        .catch(error => (this.error = error));
+    }
+  }
+};
+</script>
+
+<style scoped>
+.regform {
+  margin-top: 350px;
+}
+.input {
+  width: 260px;
+  padding: 30px;
+  margin:10px;
+}
+</style>

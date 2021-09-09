eed19b112707bb900c8d07c9c7892675b7ed2a97
Dppriest555 <ivan.velkov01@abv.bg>
2021-09-09 21:16:24 +0200

    Removed old Sign up Making new one as componenet

---
+++ workhub/src/components/InputForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="inputtext">
+           <h4>{{ title }}</h4>
+        </div>
+        <div class="inputfield">
+            <input type="email" v-model="email"/>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+  props: ['title']
+
+}
+</script>
+
+<style scoped>
+
+.container {
+    display: flex;
+    flex-direction: column;
+}
+
+input {
+    border: none;
+    border-bottom: solid 2px rgb(110, 110, 110);
+    background-color: #EEEEEE;
+    width: 80%;
+}
+
+h4 {
+    text-align: left;
+    margin-left: 10%;
+}
+
+</style>
--- workhub/src/views/Register.vue
+++ workhub/src/views/Register.vue
@@ -1,28 +1,22 @@
 <template>
-  <div class="regform">
-    <div class="error" v-if="error">{{error.message}}</div>
-    <form @submit.prevent="pressed">
-      <div class="emailtxt">
-        <h4>Email</h4>
-      </div>
-      <div class="email">
-        <input type="email"/>
-      </div>
-      <div class="passwordtxt">
-        <h4>Password</h4>
-      </div>
-      <div class="password">
-        <input type="password"/>
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
-  </div>
+  <InputForm title='Username'/>
 </template>
+
 
 <script>
 import * as firebase from "firebase/app";
 import "firebase/auth";
+
+import InputForm from '@/components/InputForm.vue'
+
+
 export default {
+
+   name: 'Register',
+  components: {
+    InputForm
+  },
+
   data() {
     return {
       email: "",
@@ -45,47 +39,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.error {
-  margin-top: 20px;
-  color: rgb(252, 70, 70);
-  font-size: 18px;
-}
-.regform{
-  margin: 5px;
-  padding: 10px 30px 40px 30px;
-  margin-top: 40%;
-  border-radius: 10px;
-  background-color: #7460aa13;
-}
-h4 {
- text-align: left;
- margin-left: 8px;
- margin-bottom: 6px;
- color: #7460AA;
- font-weight: 500;
- }
 
-.email {
-  padding-bottom: 20px;
-}
-input {
-  width: 95%;
-  font-size: 21px;
-  background-color: #7460AA;
-  border-radius: 10px;
-  border: #7460AA;
-  padding: 3px;
-  color: rgb(226, 226, 226);
-}
-button {
-  margin-top: 20%;
-  height: 45px;
-  width: 36%;
-  font-size: 100%;
-  border-radius: 27.5px;
-  color: #472C91;
-  font-weight: 700;
-  border:solid 2px #472C91;
-}
-</style>

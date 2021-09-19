9062dc21c3179c37d9e72cafec84ab3f7cf55386
JRoshthen1 <jroshthen1@gmail.com>
2021-09-19 17:23:19 +0200

    changing pic

--- workhub/src/views/Home.vue
+++ workhub/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/office-worker-1.png">
+    <HelloWorld msg="Welcome to Work"/>
   </div>
 </template>
 

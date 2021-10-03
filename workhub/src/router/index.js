0708bab7a0bc8b9305da6ab590ae867c910cbf66
Dppriest555 <ivan.velkov01@abv.bg>
2021-10-03 17:56:40 +0200

    chat

--- workhub/src/router/index.js
+++ workhub/src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Register from '../views/Register.vue';
-import Login from '../views/Login.vue'
-import CustomProfile from '../views/CustomProfile.vue'
+
+
 
 const routes = [
   {
@@ -35,7 +34,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CustomProfile.vue')
-  }
+  },
+  {
+    path: '/chatwindow',
+    name: 'ChatWindow',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ChatWindow.vue')
+  },
 ]
 
 const router = createRouter({
